fa21731c1c8f9cd3ec208c1f14c8eff272cbbf59
daniyalkozhakmet <95417472+daniyalkozhakmet@users.noreply.github.com>
2023-10-13 16:37:44 +0600

    create product

--- backend/app/Exceptions/Handler.php
+++ backend/app/Exceptions/Handler.php
@@ -3,29 +3,12 @@
 namespace App\Exceptions;
 
 use Error;
-use Illuminate\Database\Eloquent\ModelNotFoundException;
-use Illuminate\Http\Request;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Laravel\Sanctum\Exceptions\MissingAbilityException;
-use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Throwable;
 
 class Handler extends ExceptionHandler
 {
-    /**
-     * The list of the inputs that are never flashed to the session on validation exceptions.
-     *
-     * @var array<int, string>
-     */
-    protected $dontFlash = [
-        'current_password',
-        'password',
-        'password_confirmation',
-    ];
-
-    /**
-     * Register the exception handling callbacks for the application.
-     */
     public function render($request, Throwable $exception)
     {
         if ($exception instanceof Error) {
--- backend/app/Http/Controllers/AdminController.php
+++ backend/app/Http/Controllers/AdminController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\adminRequests\ProductCreateRequest;
+use App\Http\Resources\ProductWithCommentResource;
+use App\Models\Product;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
 
@@ -12,9 +14,19 @@ class AdminController extends Controller
     public function create_product(ProductCreateRequest $request)
     {
         $request->validated($request->all());
-        Storage::put('public/products', $request->image);
-        // $request->file('image')->storePublicly('products', ['disk' => 'public']);
-        
-        return $request->file('image');
+        $path = Storage::put('public/products', $request->file('image'));
+        $path = str_replace("public/", "", $path);
+        $product = Product::create([
+            "name" => $request->name,
+            "price" => $request->price,
+            "description" => $request->description,
+            "image" => 'storage/' . $path,
+        ]);
+        $product->categories()->attach(
+            $request->categories
+        );
+        $product->save();
+
+        return new ProductWithCommentResource($product);
     }
 }
